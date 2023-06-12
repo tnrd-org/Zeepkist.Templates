b331e57de1e02b900906a9f714b245bba3846a7d
Christiaan Bloemendaal <github@tnrd.net>
2023-06-12 20:13:05 +0200

    feat: initial project

---
+++ Zeepkist.Templates/templates/Zeepkist.BepInEx5.PluginTemplate/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace Zeepkist.BepInEx5.PluginTemplate;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private Harmony harmony;
+
+    private void Awake()
+    {
+        harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll();
+
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    private void OnDestroy()
+    {
+        harmony?.UnpatchSelf();
+        harmony = null;
+    }
+}
